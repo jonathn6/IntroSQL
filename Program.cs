2b622975c8cecf3b58e1a717baae66ebda6c1bb2
jonathn6 <jonathn6@icloud.com>
2021-11-20 13:08:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var arr = new int[] { 1, 2, 3, 4, 5 };
+            var returnInt = arr.Where(x => x % 2 == 0);
+
+        }
+    }
+}
